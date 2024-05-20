4155bc9cd55b8c0cf3c7f60beea0c9ba6d99efd9
Nathan Rothenberger <nrothenberger@activecampaign.com>
2024-05-20 17:20:53 -0500

    init nextra

---
+++ theme.config.jsx
@@ -0,0 +1,9 @@
+const config = {
+  logo: <span>My Nextra Documentation</span>,
+  project: {
+    link: "https://github.com/foo/bar",
+  },
+  // ... other theme options
+};
+
+export default config;
